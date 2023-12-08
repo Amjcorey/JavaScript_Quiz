5b13af3d1db77990889ec3846e3c4260ca0c8797
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2023-12-08 14:20:27 -0800

    Update script

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 // Quiz questions
+const quizQuestions = [{
+    question: "JavaScript is the same as Java. True or false?",
+    answer: [
+        {text: "True, JavaScript is a stripped-down version of Java.", isCorrect: false },
+        {text: "False, JavaScripts' syntax is loosely base on Java.", isCorrect: true }
+        ]
+}
+]
